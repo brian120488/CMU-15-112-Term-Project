53f11365a401608c7e65a508fc28de858560984d
brian120488 <brian.yang@csedge.org>
2021-08-04 14:31:22 -0400

    Created sprites

--- main/Player.py
+++ main/Player.py
@@ -1,6 +1,17 @@
 class Player(object):
-    def __init__(self):
-        pass
+    sprites: list
+    spriteCounter: int = 0
+    width: int = 20
+    height: int = 30
+
+    def __init__(self, app):
+        path = "sprites/player_sprites.png"
+        spritestrip = app.loadImage(path)
+        self.sprites = []
+        numSprites = 19
+        for i in range(numSprites):
+            sprite = spritestrip.crop((self.width * i, 0, self.width * (i + 1), self.height))
+            self.sprites.append(sprite)
 
     def __repr__(self):
-        return "hi"
+        return self.spritestrip
--- main/main.py
+++ main/main.py
@@ -2,11 +2,25 @@ from cmu_112_graphics import *
 from Player import Player
 
 def runTerraria():
-    width, height = 1920, 1080
+    width, height = 1280, 720
     runApp(width=width, height=height)
 
+def appStarted(app):
+    app.player = Player(app)
+
+def keyPressed(app):
+    pass
+
+def mousePressed(app):
+    pass
+
+def redrawAll(app, canvas):
+    for i in range(len(app.player.sprites)):
+        canvas.create_image(20 * i, 200, image=ImageTk.PhotoImage(app.player.sprites[i]), anchor="w")
+        canvas.create_line(0, 220, 20*19, 220)
+
 def main():
-    print(Player())
+    print('hi')
     runTerraria()
 
 if __name__ == "__main__":
