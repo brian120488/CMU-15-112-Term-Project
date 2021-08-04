da6db200458db8f6ecda8b6cd8b04555d9327330
brian120488 <brian.yang@csedge.org>
2021-08-04 13:39:00 -0400

    Added small touches and testing

--- main/main.py
+++ main/main.py
@@ -1,10 +1,12 @@
 from cmu_112_graphics import *
+from Player import Player
 
 def runTerraria():
     width, height = 1920, 1080
     runApp(width=width, height=height)
 
 def main():
+    print(Player())
     runTerraria()
 
 if __name__ == "__main__":
